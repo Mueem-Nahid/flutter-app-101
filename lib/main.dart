66db8df0ce89db5fd7de50a9eb516508aac4e48f
Mueem-Nahid <mueem51@gmail.com>
2024-07-27 13:23:53 +0600

    # Feature - display lists of item - add GestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+mySnackBar(message, ctx) {
+  return ScaffoldMessenger.of(ctx)
+      .showSnackBar(SnackBar(content: Text(message)));
+}
+
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
-
-  mySnackBar(message, ctx) {
-    return ScaffoldMessenger.of(ctx)
-        .showSnackBar(SnackBar(content: Text(message)));
-  }
 
   myAlertDialogue(ctx) {
     return showDialog(
@@ -281,7 +281,7 @@ class HomeActivity extends StatelessWidget {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => MyFormPage()),
+                                      builder: (context) => MyFormActivity()),
                                 );
                               },
                               style: buttonStyle,
@@ -292,7 +292,8 @@ class HomeActivity extends StatelessWidget {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => MyListViewPage()),
+                                      builder: (context) =>
+                                          MyListViewActivity()),
                                 );
                               },
                               style: buttonStyle,
@@ -316,8 +317,8 @@ class HomeActivity extends StatelessWidget {
   }
 }
 
-class MyFormPage extends StatelessWidget {
-  const MyFormPage({super.key});
+class MyFormActivity extends StatelessWidget {
+  const MyFormActivity({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -384,8 +385,39 @@ class MyFormPage extends StatelessWidget {
   }
 }
 
-class MyListViewPage extends StatelessWidget {
-  const MyListViewPage({super.key});
+class MyListViewActivity extends StatelessWidget {
+  final listItems = [
+    {
+      'id': 1,
+      'image':
+          'https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg',
+      'title': 'Iron man'
+    },
+    {
+      'id': 2,
+      'image':
+          'https://www.superherotoystore.com/cdn/shop/articles/Untitled_design_1080x.png?v=1641978925',
+      'title': 'Bat man'
+    },
+    {
+      'id': 3,
+      'image':
+          'https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/04/wonder-woman-ai-art-header.jpg',
+      'title': 'Wonder Woman'
+    },
+    {
+      'id': 4,
+      'image':
+          'https://jafcomics.com/cdn/shop/products/c56aef93-4ee8-43f5-9e14-8d95b29f4242.jpg?v=1674402657',
+      'title': 'Super girl'
+    },
+    {
+      'id': 5,
+      'image':
+          'https://loudandclearreviews.com/wp-content/uploads/2024/01/justice-league-crisis.webp',
+      'title': 'Flash'
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -393,6 +425,24 @@ class MyListViewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('List view'),
       ),
+      body: ListView.builder(
+          itemCount: listItems.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                mySnackBar(listItems[index]['title'], context);
+              },
+              child: Container(
+                margin: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 220,
+                child: Image.network(
+                  listItems[index]['image'] as String,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          }),
     );
   }
 }
