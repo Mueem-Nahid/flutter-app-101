b4a20bd93ec8da73f1f31729a2039b287151e5de
Mueem-Nahid <mueem51@gmail.com>
2024-07-27 10:24:00 +0600

    # Feature - add new page form - add form - add MaterialButton

--- lib/main.dart
+++ lib/main.dart
@@ -256,7 +256,6 @@ class HomeActivity extends StatelessWidget {
                 ),
               ),
             ),
-
             Expanded(
               flex: 3,
               child: Column(
@@ -277,6 +276,17 @@ class HomeActivity extends StatelessWidget {
                               style: buttonStyle,
                               child: const Text('Confirm dialogue'),
                             ),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MyFormPage()),
+                                );
+                              },
+                              style: buttonStyle,
+                              child: const Text('Form'),
+                            ),
                           ]),
                     ),
                   ),
@@ -290,8 +300,75 @@ class HomeActivity extends StatelessWidget {
                 ],
               ),
             )
-            // button
           ],
         ));
+  }
+}
+
+class MyFormPage extends StatelessWidget {
+  const MyFormPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Form"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'First Name'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Last Name'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Email'),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 15),
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  minWidth: double.infinity,
+                  height: 50,
+                  child: const Text('Submit'),
+                )),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 15),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                color: Colors.red,
+                textColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                minWidth: double.infinity,
+                height: 50,
+                child: const Text("Go Back!"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
