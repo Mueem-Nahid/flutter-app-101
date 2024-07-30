dd750df1bd94c90cf86ed906f71fe6a37e8b14ef
Mueem-Nahid <mueem51@gmail.com>
2024-07-30 12:41:14 +0600

    # Info - refactor - add quote_card - extract widget

--- lib/quote.dart
+++ lib/quote.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote_card.dart';
 
 class Quote {
   String text;
@@ -29,31 +30,6 @@ class _QuoteListState extends State<QuoteList> {
         text: 'The truth is rarely pure and never simple')
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.text,
-              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Text(
-              quote.author,
-              style: TextStyle(fontSize: 14, color: Colors.grey[800]),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,7 +40,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
