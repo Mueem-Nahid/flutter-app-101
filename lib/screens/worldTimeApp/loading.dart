0461df259549a0378d14bdfd75dffd946c748365
Mueem-Nahid <mueem51@gmail.com>
2024-08-01 14:23:32 +0600

    # Info - add const

--- lib/screens/worldTimeApp/loading.dart
+++ lib/screens/worldTimeApp/loading.dart
@@ -13,12 +13,12 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
   //   simulate delay, similar with setTimeout
   void simulateDelay() async {
-    String id = await Future.delayed(Duration(seconds: 3), () {
+    String id = await Future.delayed(const Duration(seconds: 3), () {
       print('fetch id');
       return '101';
     });
 
-    String details = await Future.delayed(Duration(seconds: 2), () {
+    String details = await Future.delayed(const Duration(seconds: 2), () {
       print('wait for id and then get details');
       return 'Mueem Nahid, Soft Eng.';
     });
@@ -42,7 +42,7 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text('Loading'),
     );
   }
