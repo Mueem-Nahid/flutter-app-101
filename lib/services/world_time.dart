919928d68838a7cbbbad30887c19e99ea437e121
Mueem-Nahid <mueem51@gmail.com>
2024-08-02 12:02:09 +0600

    # Info - error handling - add try catch

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class WorldTime {
@@ -10,15 +11,19 @@ class WorldTime {
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    // https://worldtimeapi.org/api/timezone/Europe/London
-    var endpoint =
-        Uri.https('worldtimeapi.org', '/api/timezone/$url', {'q': '{https}'});
-    var response = await http.get(endpoint);
-    Map data = jsonDecode(response.body);
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    time = now.toString();
+    try {
+      // https://worldtimeapi.org/api/timezone/Europe/London
+      var endpoint =
+          Uri.https('worldtimeapi.org', '/api/timezone/$url', {'q': '{https}'});
+      var response = await http.get(endpoint);
+      Map data = jsonDecode(response.body);
+      String dateTime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      DateTime now = DateTime.parse(dateTime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      time = now.toString();
+    } catch (e) {
+      time = 'Failed to get time data';
+    }
   }
 }
